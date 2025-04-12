7eb6f7c76062ccc55415ba237b01daf3cdc584c1
Ismail Amr <68963264+Ismail1909@users.noreply.github.com>
2025-04-12 21:00:49 +0200

    implement malloc container to store heap allocated data
    - malloc struct to hold diagnostic data
    - custom set variable to store heap allocation entries for tracing allocations

--- Labs/memory-leak-detector/new.h
+++ Labs/memory-leak-detector/new.h
@@ -5,6 +5,39 @@
 #include <unordered_set>
 #include <memory>
 
+struct malloc_struct {
+	malloc_struct(void* paddr, size_t bytes, bool is_array, const char* file = " ", const char* func = " ", int line = -1) :
+		m_paddr{ paddr }, m_bytes{ bytes }, m_is_array{ is_array }, m_file{ file }, m_func{ func }, m_line{ line } {
+	}
+	void* m_paddr;
+	size_t m_bytes;
+	const char* m_file;
+	const char* m_func;
+	int	  m_line;
+	bool  m_is_array;
+};
+
+struct malloc_hash : std::hash<void *> {
+	size_t operator()(const malloc_struct& p) const {
+		return std::hash<void*>::operator()(p.m_paddr);
+	}
+};
+
+template<typename T>
+struct malloc_allocator_t : std::allocator<T> {
+	T* allocate(std::size_t n) {
+		T* p = (T*)std::malloc(n);
+		if (!p) throw std::bad_alloc();
+
+		return p;
+	}
+
+	void deallocate(T* p, std::size_t n) {
+		std::free(p);
+	}
+};
+
+std::unordered_set<malloc_struct, malloc_hash, std::equal_to<malloc_hash>,malloc_allocator_t<malloc_struct>> heap_entry_set;
 
 void* operator new(size_t size) {
 	std::cout << "new operator called" << std::endl;
